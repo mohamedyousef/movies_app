78070bd3a990bdd7313f57cefc3e10716c469abe
mohamedyousef <mohamedyousef106@gmail.com>
2023-10-06 20:05:35 +0300

    In this commit implemented  config file - added apiKey for testing purpose - baseUrlStaging - baseUrlProd - baseUrlDev

---
+++ lib/core/config/config.dart
@@ -0,0 +1,7 @@
+import 'package:instbug_task/di_module/di_module.dart';
+
+const Enviroment enviroment = Enviroment.dev;
+const apiKey = "02aee42027f611a479b0b333d3ed64f1";
+const baseUrlStaging = "https://api.themoviedb.org/3/";
+const baseUrlProd = "https://api.themoviedb.org/3/";
+const baseUrlDev = "https://api.themoviedb.org/3/";
